24a58b981bd753f7ed975c9972870e17cb4d4fa7
FerreWagner <18408229270@163.com>
2018-03-26 17:49:29 +0800

    centos dll prepare

---
+++ centos_set.php
@@ -0,0 +1,21 @@
+<?php
+// echo phpinfo();
+// /usr/local/postgresql/include
+// ln -s /usr/local/postgresql/include/* /usr/local/include/
+
+// /usr/local/php/bin/phpize
+// /usr/local/php7/bin/phpize
+
+// ./configure -with-php-config=/usr/local/php7/bin/php-config  
+// ./configure –with-php-config=/usr/local/php7/bin/php-config
+// ./configure -with-php-config=/usr/bin/php-config  
+
+
+
+// centos6 pgsql扩展安装命令
+// tar -xzvf PDO_PGSQL-1.0.tgz
+// 进入底层目录后:
+// /usr/local/php7/bin/phpize
+// ./configure -with-php-config=/usr/local/php7/bin/php-config 
+
+?>
